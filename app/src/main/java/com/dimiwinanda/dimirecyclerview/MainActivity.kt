9e6373149d505872f220df68dd8b2165a4572b5f
Dimi Winanda <102844406+dimiwinanda1@users.noreply.github.com>
2022-04-27 15:10:07 +0700

    done1

--- app/src/main/java/com/dimiwinanda/dimirecyclerview/MainActivity.kt
+++ app/src/main/java/com/dimiwinanda/dimirecyclerview/MainActivity.kt
@@ -1,21 +1,30 @@
+//untuk mendeklarasikan package com.dimiwinanda.dimirecyclerview kita dapat menggunakan kata kunci package.
 package com.dimiwinanda.dimirecyclerview
 
+//kita dapat menggunakan deklarasi import untuk mengaktifkan compiler untuk menemukan class, function, interface atau object yang akan diimpor.
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+//Class MainActivity merupakan turunan dari Class AppCompatActivity dan mengimplementasikan Interface View.
 class MainActivity : AppCompatActivity() {
     private val itemsList = ArrayList<String>()
     private lateinit var customAdapter: CustomAdapter
 
+    //onCreate() adalah kondisi awal saat Activity baru diciptakan, disin dilakukan inisialisasi.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "Dimi RecyclerView"
-
+        
+        //RecyclerView meminta tampilan tersebut, dan mengikat tampilan ke data, dengan memanggil metode di adaptor. 
+        //dalam file layout aktivitas akan berfungsi sebagai kontainer tampilan.
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         customAdapter = CustomAdapter(itemsList)
+        
+        //Instance dari RecyclerView.LayoutManager untuk menyusun layout item dalam RecyclerView. 
+        //Pengelola layout ini berfungsi untuk mengelola susunan item daftar di dalamnya. 
         val layoutManager = LinearLayoutManager(applicationContext)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = customAdapter
